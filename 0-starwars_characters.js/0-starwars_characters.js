5f0390e120904370f982ee6ab7740a73e5921d90
vundla <vundlamandlenkosi0@gmail.com>
2024-09-19 06:48:04 -0700

    0-starwars_characters.js

--- 0-starwars_characters.js/0-starwars_characters.js
+++ 0-starwars_characters.js/0-starwars_characters.js
@@ -1,33 +1,54 @@
 #!/usr/bin/node
-{
-	"name": "Luke Skywalker",
-	"height": "172",
-	"mass": "77",
-	"hair_color": "blond",
-	"skin_color": "fair",
-	"eye_color": "blue",
-	"birth_year": "19BBY",
-	"gender": "male",
-	"homeworld": "https://swapi-api.alx-tools.com/api/planets/1/",
-	"films": [
-		"https://swapi-api.alx-tools.com/api/films/2/",
-		"https://swapi-api.alx-tools.com/api/films/6/",
-		"https://swapi-api.alx-tools.com/api/films/3/",
-		"https://swapi-api.alx-tools.com/api/films/1/",
-		"https://swapi-api.alx-tools.com/api/films/7/"
-	],
-	"species": [
-		"https://swapi-api.alx-tools.com/api/species/1/"
-	],
-	"vehicles": [
-		"https://swapi-api.alx-tools.com/api/vehicles/14/",
-		"https://swapi-api.alx-tools.com/api/vehicles/30/"
-	],
-	"starships": [
-		"https://swapi-api.alx-tools.com/api/starships/12/",
-		"https://swapi-api.alx-tools.com/api/starships/22/"
-	],
-	"created": "2014-12-09T13:50:51.644000Z",
-	"edited": "2014-12-20T21:17:56.891000Z",
-	"url": "https://swapi-api.alx-tools.com/api/people/1/"
-}
+import requests
+import sys
+
+def get_movie_data(The Phantom Menace):
+    # Base URL for Star Wars API
+    url = f"https://swapi.dev/api/films/{The Phantom Menace}/"
+
+    # Send request to get the movie data
+    response = requests.get(url)
+
+    # If the response status is not 200, return None (movie doesn't exist)
+    if response.status_code != 200:
+        print(f"Error: Movie with ID {The Phantom Menace} not found")
+        return None
+
+    return response.json()
+
+def get_character_name(character_url):
+    # Send request to get the character data
+    response = requests.get(character_url)
+
+    # Return the character name if request is successful
+    if response.status_code == 200:
+        return response.json().get("name")
+    return None
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py <The Phantom Menace>")
+        sys.exit(1)
+
+    # Get the movie ID from the first argument
+    movie_id = sys.argv[1]
+
+    # Get movie data from SWAPI
+    movie_data = get_movie_data(movie_id)
+
+    # If movie data is None, exit
+    if movie_data is None:
+        sys.exit(1)
+
+    # Get list of character URLs from the movie data
+    character_urls = movie_data.get("characters", [])
+
+    # Print each character name
+    for character_url in character_urls:
+        character_name = get_character_name(character_url)
+        if character_name:
+            print(character_name)
+
+if __name__ == "__main__":
+    main()
+
